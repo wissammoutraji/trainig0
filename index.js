6e0b9d7642d053b3cce30e35f6e3d92c79aaac3f
wissammoutraji <wissammoutraji@gmail.com>
2021-02-20 00:50:18 +0200

    countries

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ fetch('https://restcountries.eu/rest/v2/all')
 <div class="card">
 <img src="sawsaw" class="card-img-top" alt="...">
 <div class="card-body">
-<h5 class="card-title">Country name: ahmad</h5>
-<p class="card-text">Capital is: samir </p>
-<p class="card-text"><small class="text-muted">Population is sondos</small></p>
+<h5 class="card-title">Country name: Names</h5>
+<h5 class="card-title">Native name: Natives</h5>
+<p class="card-text">Capital is: Caps </p>
+<p class="card-text">Region is: Regs </p>
+<p class="card-text"><small class="text-muted">Population is PoP</small></p>
 </div>
 `
 
@@ -21,12 +23,13 @@ fetch('https://restcountries.eu/rest/v2/all')
         
       
       var x = document.createElement('div')
-      x.className = "col-lg-4 col-md-6";
+      x.className = "col-lg-3 col-md-4  col-sm-6";
       var z = content.replace("sawsaw", json[i].flag)
-      z = z.replace("ahmad", json[i].name)
-      z = z.replace("samir", json[i].capital)
-      z = z.replace("sondos", json[i].population)
-
+      z = z.replace("Names", json[i].name)
+      z = z.replace("Caps", json[i].capital)
+      z = z.replace("PoP", json[i].population)
+      z = z.replace("Natives", json[i].nativeName)
+      z = z.replace("Regs", json[i].region)
       x.innerHTML = z
       document.getElementById('insertimage').appendChild(x) }
     }
